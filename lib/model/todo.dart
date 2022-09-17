45d16a0ceafa60ccb9902b99093c8f19749be128
wqiu68 <wqiu68@gmail.com>
2022-09-16 23:28:57 -0400

    updated

--- lib/model/todo.dart
+++ lib/model/todo.dart
@@ -11,12 +11,24 @@ class ToDo {
 
   static List<ToDo> todoList() {
     return [
-      ToDo(id: '01', todoText: 'Morning Excercise', isDone: true ),
-      ToDo(id: '02', todoText: 'Buy Groceries', isDone: true ),
-      ToDo(id: '03', todoText: 'Check Emails', ),
-      ToDo(id: '04', todoText: 'Team Meeting', ),
-      ToDo(id: '05', todoText: 'Work on mobile apps for 2 hour', ),
-      ToDo(id: '06', todoText: 'Dinner with Jenny', ),
+      ToDo(id: '01', todoText: 'Go on morning run', isDone: true),
+      ToDo(
+        id: '02',
+        todoText: 'Recruit prep',
+      ),
+      ToDo(
+        id: '03',
+        todoText: 'Call Mom and Dad',
+      ),
+      ToDo(
+        id: '04',
+        todoText: 'Team Meeting',
+      ),
+      ToDo(
+        id: '05',
+        todoText: 'Ceramics class this weekend',
+      ),
+      ToDo(id: '06', todoText: 'Dinner with roommates', isDone: true),
     ];
   }
 }
